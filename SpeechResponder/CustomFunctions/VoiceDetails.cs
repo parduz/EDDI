cc51769831496ce07ccf8fdb1951f35e08e6722e
Tkael <dragoncharmer84@hotmail.com>
2021-01-13 20:59:56 -0800

    Add the Windows.Media.SpeechSynthesis synthesizer to open additional voices for use.

--- SpeechResponder/CustomFunctions/VoiceDetails.cs
+++ SpeechResponder/CustomFunctions/VoiceDetails.cs
@@ -1,11 +1,10 @@
-﻿using Cottle.Functions;
+﻿using System;
+using Cottle.Functions;
 using Cottle.Values;
 using EddiSpeechResponder.Service;
 using EddiSpeechService;
 using JetBrains.Annotations;
-using System.Collections.Generic;
 using System.Linq;
-using System.Speech.Synthesis;
 
 namespace EddiSpeechResponder.CustomFunctions
 {
@@ -19,68 +18,18 @@ namespace EddiSpeechResponder.CustomFunctions
         {
             if (values.Count == 0)
             {
-                List<VoiceDetail> voices = new List<VoiceDetail>();
-                if (SpeechService.Instance?.allVoices != null)
-                {
-                    foreach (VoiceInfo vc in SpeechService.Instance.allVoices.Select(v => v.voiceInfo))
-                    {
-                        if (!vc.Name.Contains("Microsoft Server Speech Text to Speech Voice"))
-                        {
-                            voices.Add(new VoiceDetail(
-                                vc.Name,
-                                vc.Culture.Parent.EnglishName,
-                                vc.Culture.Parent.NativeName,
-                                vc.Culture.Name,
-                                vc.Gender.ToString()
-                                ));
-                        }
-                    }
-                }
-                return new ReflectionValue(voices);
+                return new ReflectionValue(SpeechService.Instance?.allVoices);
             }
             if (values.Count == 1)
             {
-                VoiceDetail result = null;
+                EddiSpeechService.VoiceDetails result = null;
                 if (SpeechService.Instance?.allVoices != null && !string.IsNullOrEmpty(values[0].AsString))
                 {
-                    foreach (VoiceInfo vc in SpeechService.Instance.allVoices.Select(v => v.voiceInfo))
-                    {
-                        if (vc.Name.ToLowerInvariant().Contains(values[0].AsString.ToLowerInvariant())
-                        && !vc.Name.Contains("Microsoft Server Speech Text to Speech Voice"))
-                        {
-                            result = new VoiceDetail(
-                                vc.Name,
-                                vc.Culture.Parent.EnglishName,
-                                vc.Culture.Parent.NativeName,
-                                vc.Culture.Name,
-                                vc.Gender.ToString()
-                                );
-                            break;
-                        }
-                    }
+                    result = SpeechService.Instance?.allVoices.SingleOrDefault(v => string.Equals(v.name, values[0].AsString, StringComparison.InvariantCultureIgnoreCase));
                 }
                 return new ReflectionValue(result ?? new object());
             }
             return "The VoiceDetails function is used improperly. Please review the documentation for correct usage.";
         }, 0, 1);
-    }
-
-    [PublicAPI]
-    class VoiceDetail
-    {
-        public string name { get; }
-        public string cultureinvariantname { get; }
-        public string culturename { get; }
-        public string culturecode { get; }
-        public string gender { get; }
-
-        public VoiceDetail(string name, string cultureinvariantname, string culturename, string culturecode, string gender)
-        {
-            this.name = name;
-            this.cultureinvariantname = cultureinvariantname;
-            this.culturename = culturename;
-            this.culturecode = culturecode;
-            this.gender = gender;
-        }
     }
 }
